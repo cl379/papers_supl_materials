09671f5c1c8875a85cae8570cd1a30240b5a87d7
Carla Lancelotti <carla.lancelotti@upf.edu>
2015-06-01 13:10:49 +0200

    repo IMPR created

--- Lancelotti2015fuel/PCAs_script.R
+++ Lancelotti2015fuel/PCAs_script.R
@@ -32,10 +32,7 @@ alm.chem <- read.table("alm_chem.csv", header = TRUE)
 
 
 ##### PCA #####
-
-
 ### Charcoal ###
- 
 ## PCA on charcoal data per site (all sites) ##
 res.pca<-PCA(charc.site[,1:8], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/charcoal_sites.pdf")
@@ -45,7 +42,6 @@ ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Anthraco-remains")
 mtext("", outer=TRUE, line=-1.5, font=2, adj=0.5, cex=1.25)
 dev.off()
-
 # Harappa charcoal
 res.pca<-PCA(har.char[,1:6], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/charcoal_harappa.pdf")
@@ -54,7 +50,6 @@ concat<-cbind.data.frame(har.char[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Harappa Anthraco-remains")
 dev.off()
-
 # Kanmer charcoal
 res.pca<-PCA(kmr.char[,1:7], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/charcoal_kanmer.pdf")
@@ -63,7 +58,6 @@ concat<-cbind.data.frame(kmr.char[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Kanmer Anthraco-remains")
 dev.off()
-
 # Shikarpur charcoal
 res.pca<-PCA(skp.char[,1:6], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/charcoal_shikarpur.pdf")
@@ -72,7 +66,6 @@ concat<-cbind.data.frame(skp.char[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Shikarpur Anthraco-remains")
 dev.off()
-
 # Alamgirpur charcoal
 res.pca<-PCA(alm.char[,1:5], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/charcoal_alamgirpur.pdf")
@@ -83,7 +76,6 @@ plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Alamgirpur
 dev.off()
 
 ### Phytoliths ###
-
 ## All sites (per site) ##
 res.pca<-PCA(phy.site[,1:5], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/phytoliths_sites.pdf")
@@ -92,7 +84,6 @@ concat<-cbind.data.frame(phy.site[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Phytoliths")
 dev.off()
-
 # Dung phytoliths
 res.pca<-PCA(dun.phy[,1:5], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/phytoliths_dung.pdf")
@@ -101,7 +92,6 @@ concat<-cbind.data.frame(dun.phy[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Phytoliths dung")
 dev.off()
-
 # Harappa phytoliths
 res.pca<-PCA(har.phy[,1:5], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/phytoliths_harappa.pdf")
@@ -110,7 +100,6 @@ concat<-cbind.data.frame(har.phy[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Phytoliths Harappa")
 dev.off()
-
 # Kanmer phytoliths
 res.pca<-PCA(kmr.phy[,1:5], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/phytoliths_Kanmer.pdf")
@@ -119,7 +108,6 @@ concat<-cbind.data.frame(kmr.phy[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Phytoliths Kanmer")
 dev.off()
-
 # Shikarpur phytoliths
 res.pca<-PCA(skp.phy[,1:5], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/phytoliths_shikarpur.pdf")
@@ -128,7 +116,6 @@ concat<-cbind.data.frame(skp.phy[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Phytoliths Shikarpur")
 dev.off()
-
 # Alamgirpur phytoliths
 res.pca<-PCA(alm.phy[,1:5], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/phytoliths_alamgirpur.pdf")
@@ -140,7 +127,6 @@ dev.off()
 
 
 ### Chemicals ###
-
 ## All sites (per site) ##
 res.pca<-PCA(chem.site[,1:36], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/chemical_all.pdf")
@@ -149,8 +135,6 @@ concat<-cbind.data.frame(chem.site[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Multielement")
 dev.off()
-
-
 # Dung multielement
 res.pca<-PCA(dun.chem[,1:36], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/chemical_dung.pdf")
@@ -159,7 +143,6 @@ concat<-cbind.data.frame(dun.chem[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Multielement dung")
 dev.off()
-
 # Harappa multielement
 res.pca<-PCA(har.chem[,1:36], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/chemical_harappa.pdf")
@@ -168,7 +151,6 @@ concat<-cbind.data.frame(har.chem[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Multielement Harappa")
 dev.off()
-
 # Kanmer multielement
 res.pca<-PCA(kmr.chem[,1:36], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/chemical_kanmer.pdf")
@@ -177,7 +159,6 @@ concat<-cbind.data.frame(kmr.chem[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Multielement Kanmer")
 dev.off()
-
 # Shikarpur multielement
 res.pca<-PCA(skp.chem[,1:36], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/chemical_shikarpur.pdf")
@@ -186,7 +167,6 @@ concat<-cbind.data.frame(skp.chem[,1], res.pca$ind$coord)
 ellipse.coord<-coord.ellipse(concat, bary=TRUE)
 plot.PCA(res.pca, habillage=1, ellipse=ellipse.coord, cex=0.8, title="Multielement Shikarpur")
 dev.off()
-
 # Alamgirpur multielement
 res.pca<-PCA(alm.chem[,1:36], scale.unit=TRUE, ncp=5, quali.sup=1, graph=FALSE)
 pdf("~/Documents/R/phd/percentiles/plots2/chemical_alamgirpur.pdf")
